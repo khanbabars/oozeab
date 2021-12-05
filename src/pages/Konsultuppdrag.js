cff938bcc729d1ae33d0526515d5d91075fd1b86
khanbabars <khanbabars@gmail.com>
2021-12-05 21:34:54 +0100

    fixed uppdrag

--- src/pages/Konsultuppdrag.js
+++ src/pages/Konsultuppdrag.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { MDBContainer } from 'mdbreact';
 // import AboutText from '../blogs/about'
 import services from '../images/services.PNG';
 import Assignments from '../api/Assignments';
-
+import Assignment_detail from '../api/Assignment_detail';
 
 
 
 export const Konsultuppdrag = () => { 
-    
+ 
+
+
   return(
 
 
@@ -17,12 +19,9 @@ export const Konsultuppdrag = () => {
         <div>
    
         <br/><br/><br/>
-        <hr/>    <p style={{ paddingTop: '20px',fontSize: '30px', color: '#434952', textAlign:'left'}}><ul>Hitta konsultuppdrag</ul></p>     <hr/>  
-        <br/><br/> 
-        &nbsp;&nbsp;<Assignments/>
-        <br/><br/><br/><br/>
-      
-        
+        <p style={{ paddingTop: '20px',fontSize: '32px', color: 'black', textAlign:'left'}}><ul>Hitta konsultuppdrag</ul></p>     <hr/>  
+        <br/>
+        &nbsp;&nbsp; <Assignments/> 
         <img src={services} alt={"services"} /><br/>
         
         <br/> <br/>      <br/> <br/>      <br/> <br/>      <br/> <br/>      <br/> <br/>      <br/> <br/>
