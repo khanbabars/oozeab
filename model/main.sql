78b0e4a257fc02ce7daf50fa3aa51d2b9bae5896
khanbabars <khanbabars@gmail.com>
2022-03-08 17:38:03 +0100

    changed column lenght project_updraged

--- model/main.sql
+++ model/main.sql
@@ -23,7 +23,7 @@ project_contact               varchar2(4000),
 project_url                   varchar2(1000),
 project_start_date            varchar2(200),
 project_end_date              varchar2(200),
-project_availablity           varchar2(20),
+project_availablity           varchar2(500),
 project_location              varchar2(2000),
 application_close_date        varchar2(200),
 dump_load_id                  number   
